19b4759e0f97afa1295d01964a6efeef87ae78f8
Fadl-Matar <s195846@win.dtu.dk>
2020-01-15 13:45:44 +0100

    Slet af land on field metode

--- src/main/java/fields/GoToJail.java
+++ src/main/java/fields/GoToJail.java
@@ -5,16 +5,10 @@ import models.Player;
 
 import java.awt.*;
 
-public class GoToJail extends Field {
+public class  GoToJail extends Field {
 
     public GoToJail(String name, Color color) {
         super(name, color);
     }
 
-    public void landOnGoToJail (Player player) {
-
-        player.getAccount().deposit(0);
-
-
-    }
 }
